3c693a275b3d52724ed590da97474543defb3f14
Emma Mattner <emattner1997@gmail.com>
2021-09-01 16:38:52 +0930

    past work and skills

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,21 @@ import React from "react";
 export default function Navbar() {
     return (
         <header>
-            
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a className="title-font font-medium text-white mb-4 md:mb-0">
+                    <a href="#about" className="ml-3 text-xl">
+                        Emma Mattner
+                </a>
+                </a>
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+                    <a href="#projects" className="mr-5 hover:text-white">
+                        Past Work
+                    </a>
+                    <a href="#skills" className="mr-5 hover:text-white">
+                        Skills
+                    </a>
+                </nav>
+                </div>
         </header>
     );
 }
