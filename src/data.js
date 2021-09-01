cd7704b5015229194949e2474df3217736441680
Emma Mattner <emattner1997@gmail.com>
2021-09-01 16:39:32 +0930

    updated descriptions

--- src/data.js
+++ src/data.js
@@ -9,21 +9,21 @@ export const projects = [
   {
     title: "Perfect Pooch Finder",
     description:
-      "",
+      "The Perfect pooch finder allows the user to enter details about themselves to generate an appropriate breed match to the users lifestyle",
     image: "perfpooch3.png",
     link: "https://perfect-pooch-finder.herokuapp.com/",
   },
   {
     title: "Tech Blog",
     description:
-      "",
+      "This is a tech blog that the user can use to post and edit their thoughts",
     image: "./tech-blog2.png",
     link: "https://emmas-tech-blog.herokuapp.com/",
   },
   {
     title: "Recipe Scraper",
     description:
-      "",
+      "The Recipe Scraper is used to scrape recipes from your favourite websites allowing the user to store them in their own personal online cookbook",
     image: "./recipe-scraper.png",
     link: "https://recipe-scraper-peg.herokuapp.com/",
   },
