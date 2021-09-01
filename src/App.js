effaeeb8cf43c3d389a2c934ac5164d5757ef931
Emma Mattner <emattner1997@gmail.com>
2021-09-01 16:39:21 +0930

    imported skills

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
+import { skills } from "./data";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Navbar />
       <About />
       <Projects />
+      <Skills />
       <Contact />
     </main>
   );
