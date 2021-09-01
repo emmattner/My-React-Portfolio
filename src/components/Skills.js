73ad33f8f7dd7b25b17cdd2cd28bbc8b27137ba8
Emma Mattner <emattner1997@gmail.com>
2021-09-01 16:33:51 +0930

    skill list

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,7 +16,19 @@ export default function Skills() {
                         Here are some skills I developed whilst studying at the University of Adelaide
                     </p>
                 </div>
+                <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                    {skills.map((skill) => (
+                        <div key={skill} className="p-2 sm:w-1/2 w-full">
+                            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                                <span className="title-font font-medium text-white">
+                                    {skill}
+                                </span>
+                            </div>
+                        </div>
+                    ))}
                 </div>
+            </div>
         </section>
     );
 }
